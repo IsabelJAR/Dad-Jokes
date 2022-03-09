f2d60fd01f00f3aadf7d60940663ec4dfbdbea94
IsabelJAR <92735909+IsabelJAR@users.noreply.github.com>
2022-03-09 11:24:40 -0500

    uploaded index.html, style.css, and joke list
    joke list idea was scrapped later on

---
+++ joke-list.js
@@ -0,0 +1,17 @@
+
+  joke1 = {
+    joke: 'Why did the scarecrow win an award?',
+    punchLine: 'Because he was outstanding in his field.'
+  };
+  joke2= {
+    joke: 'What is the difference between a poorly dressed man on a tricycle and a well-dressed man on a bicycle?',
+    punchLine: 'Attire'
+  };
+  joke3= {
+    joke: "What is a ninjas favorite type of shoes?",
+    punchLine: 'Sneakers'
+  };
+  joke4= {
+    joke: 'Last night I had a dream that I weighed less than a thousandth of a gram',
+    punchLine: 'I was like 0mg'
+  };
